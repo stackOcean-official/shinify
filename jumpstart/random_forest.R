7ac5b2d2f56457b5faa822c3ed288b55c437e91d
Christopher <christopher@stackocean.io>
2022-10-20 10:49:46 +0200

    changed var name to match conventions

--- jumpstart/random_forest.R
+++ jumpstart/random_forest.R
@@ -15,8 +15,8 @@ data_train <- data[1:(nrow(data) - 100), ]
 data_test <- data[(nrow(data) - 99):nrow(data), ]
 
 # actual random forest
-mod_rf <- randomForest(legendary ~ attack + defense, data = data_train)
-summary(mod_rf)
+rf_mod <- randomForest(legendary ~ attack + defense, data = data_train)
+summary(rf_mod)
 
 # input for new prediction
 attack <- 120
@@ -24,7 +24,7 @@ defense <- 290
 test_data_new <- data.frame(attack, defense)
 
 # actual prediction that pokemon is legendary
-predict(mod_rf, newdata = data_test)
+predict(rf_mod, newdata = data_test)
 
 # shinify random forest model
-shinify(mod_rf, modeltype = "rf", title = "your title here")
+shinify(rf_mod, modeltype = "rf", title = "your title here")
