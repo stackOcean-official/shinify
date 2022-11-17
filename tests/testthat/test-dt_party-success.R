7fb57d71845a27a2fa09e8b4c2b35f4dcf4d81eb
Christopher <christopher@stackocean.io>
2022-11-17 10:48:03 +0100

    more test scenarios

---
+++ tests/testthat/test-dt_party-success.R
@@ -0,0 +1,19 @@
+test_that("test dt_party success", {
+  install.packages("party", repos = "http://cran.us.r-project.org")
+  library(party)
+  data <- read.csv("https://github.com/stackOcean-official/hostr/files/9681827/pokemon.csv")
+
+  # create variables
+  legendary <- data$is_legendary
+  attack <- data$attack
+  defense <- data$defense
+
+  # split train and test data
+  data <- data.frame(legendary, attack, defense)
+  data_train <- data[1:(nrow(data) - 100), ]
+  # actual linear regression
+  dt <- ctree(legendary ~ attack + defense, data = data_train)
+  expect_no_error(
+    shinify(dt, modeltype = "dt_party", title = "your title here", attr_names = c("legendary", "attack", "defense"), attr_types = c("num", "num", "num"))
+  )
+})
