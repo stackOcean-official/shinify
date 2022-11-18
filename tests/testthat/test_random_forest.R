54cfd8ba8764c71940a11d0e9f14218bfc58bbc6
Jonas Mielck <jonasmielck@Jonass-MacBook-Pro.local>
2022-11-18 21:37:38 +0100

    test for random forest added

---
+++ tests/testthat/test_random_forest.R
@@ -0,0 +1,16 @@
+test_that("Test no Error", {
+  data <- read.csv("https://github.com/stackOcean-official/hostr/files/9681827/pokemon.csv")
+
+  # create variables
+  legendary <- data$is_legendary
+  attack <- data$attack
+  defense <- data$defense
+
+  # split train and test data
+  data <- data.frame(legendary, attack, defense)
+  data_train <- data[1:(nrow(data) - 100), ]
+
+  # actual random forest
+  rf_mod <- randomForest(legendary ~ attack + defense, data = data_train)
+  expect_no_error(shinify(rf_mod))
+})
