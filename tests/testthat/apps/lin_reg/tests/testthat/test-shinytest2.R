a7ce435eada67d917a470fa4c1730e68dd2a7824
Jonas Mielck <jonasmielck@Jonass-MBP.fritz.box>
2022-11-25 15:31:30 +0100

    testing infrastructure partially set up

--- tests/testthat/apps/lin_reg/tests/testthat/test-shinytest2.R
+++ tests/testthat/apps/lin_reg/tests/testthat/test-shinytest2.R
@@ -3,10 +3,10 @@ library(shinytest2)
 test_that("{shinytest2} recording: lin_reg", {
   app <- AppDriver$new(name = "lin_reg", height = 685, width = 899)
   app$expect_values()
-  app$set_inputs(num1 = 1)
-  app$set_inputs(num1 = character(0))
   app$set_inputs(num1 = 190)
-  app$set_inputs(num2 = character(0))
-  app$set_inputs(num2 = 70)
+  app$set_inputs(num2 = 90)
 })
+
+
+
 
