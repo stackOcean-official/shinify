93950a590dd4dfce019527e3b831949cdd033337
Jonas Mielck <jonasmielck@Jonass-MBP.fritz.box>
2022-11-25 15:00:27 +0100

    setting up shinytest2

--- get_to_know_shinytest2.R
+++ get_to_know_shinytest2.R
@@ -1,5 +1,7 @@
-setwd("~/Documents/dev/business/shinify/jumpstart")
+setwd("~/Documents/dev/business/shinify/tests/testthat/apps/lin_reg")
 #install.packages("shinytest2")
 library(shinytest2)
 
+
 record_test()
+
